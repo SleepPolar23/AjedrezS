72d81efeccc22a75cccb0f2cecc0c9f5aba8d3cd
ignacio-magno <lasterriens@gmail.com>
2023-03-09 22:19:19 -0300

    with test

---
+++ TestProject1/Tests.cs
@@ -0,0 +1,22 @@
+﻿using Ajedrez.Domain;
+using NUnit.Framework;
+
+namespace TestProject1;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void Tablero_VerPosiblesMovimientos()
+    {
+        var game = new Game();
+        var mov = new Mov().PosiblesMovimientos(1, 1);
+        var res = game.MakeMov(mov);
+
+        res.PositionMov[0].X.Should().Be(0);
+        res.PositionMov[0].Y.Should().Be(2);
+        
+        res.PositionMov[1].X.Should().Be(2);
+        res.PositionMov[1].Y.Should().Be(2);
+    }
+}
